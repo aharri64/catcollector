a315e8e1664ac8cf207a4a08d5273c50d1f4fd64
aharri64 <haveacompleteday@gmail.com>
2021-03-25 13:14:57 -0600

    add urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('cats/', views.cats_index, name='cats'),
     path('cats/<int:cat_id>/', views.cats_show, name='cats_show'),
-    path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
+    path('cats/create/', views.cats_new, name='cats_create'),
     path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
     path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
     path('cats/<int:pk>/add_feeding/', views.add_feeding, name='add_feeding'),
+    path('accounts/signup', views.signup, name='signup')
 ]
